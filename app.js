938d5a39112280b66bedfff8bcb66239edaf8dbe
Rasujon3 <rasujon3@gmail.com>
2022-02-09 20:30:40 +0600

    counter with function

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const allButton = document.getElementsByClassName("buton-test");
-const title = document.getElementById("title");
-const prime = document.getElementById("prime");
+// const allButton = document.getElementsByClassName("buton-test");
+// const title = document.getElementById("title");
+// const prime = document.getElementById("prime");
 
-for (const button of allButton) {
-  button.addEventListener("click", function (e) {
-    if (e.target.id == "prime") {
-      title.style.color = "red";
-    } else {
-      title.style.color = "green";
-    }
-  });
+// for (const button of allButton) {
+//   button.addEventListener("click", function (e) {
+//     if (e.target.id == "prime") {
+//       title.style.color = "red";
+//     } else {
+//       title.style.color = "green";
+//     }
+//   });
+// }
+
+let count = 0;
+
+function add(e) {
+  count++;
+  control("count");
+}
+function minus(e) {
+  if (count > 0) {
+    count--;
+    control("count");
+  } else {
+    count = 0;
+  }
+}
+
+function control(id) {
+  document.getElementById(id).innerText = count;
 }
